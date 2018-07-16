3cd2cc52a3b5b50cf803eefd75fcafe81814a760
Andy Shen <andyshenproper@gmail.com>
2018-07-16 15:39:10 -0700

    Basic Framework

---
+++ discot/src/main/java/git/inhostudios/dtba/Globals.java
@@ -0,0 +1,8 @@
+package git.inhostudios.dtba;
+
+public class Globals {
+
+	public static final String token = "";
+	public static final String prefix = "dtba>";
+	
+}
---
+++ discot/src/main/java/git/inhostudios/dtba/Main.java
@@ -0,0 +1,31 @@
+package git.inhostudios.dtba;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Main extends ListenerAdapter {
+
+	public static void main(String[] args) throws Exception{
+		JDA jda = new JDABuilder(AccountType.BOT).setToken(Globals.token).buildBlocking();
+		jda.addEventListener(new Main());
+	}
+	
+	public void onMessageReceived(MessageReceivedEvent event) {
+		if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContentDisplay());
+        }
+	}
+
+}
