4413c988a43d0990980d1962e72ade184fdf6313
Andy Shen <andyshenproper@gmail.com>
2018-07-20 12:19:21 -0700

    Working on location code

--- discot/src/main/java/git/inhostudios/dtba/gameobjects/Location.java
+++ discot/src/main/java/git/inhostudios/dtba/gameobjects/Location.java
@@ -1,5 +1,13 @@
 package git.inhostudios.dtba.gameobjects;
 
+import java.util.ArrayList;
+
 public class Location {
 
+	private ArrayList<SubLocation> spots = new ArrayList<SubLocation>();
+	
+	public Location(ArrayList<SubLocation> spots) {
+		this.spots = spots;
+	}
+	
 }
